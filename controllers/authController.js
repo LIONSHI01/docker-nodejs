f20f06829262af7583369ca7ce4dafabcdad93c9
Lion Shi <shizijian2010@yahoo.com.hk>
2023-02-12 14:16:30 +0800

    add mongodb, postRoute, authRoute

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+exports.signup = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // encrypt password
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await User.create({
+      username,
+      password: hashPassword,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  try {
+    if (!user) {
+      res.status(404).json({
+        status: "fail",
+        message: "user not found",
+      });
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isCorrect) {
+      res.status(404).json({
+        status: "fail",
+        message: "Wrong username or password",
+      });
+    }
+
+    res.status(201).json({
+      status: "success login",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
+
+const {
+  MONGO_USER,
+  MONGO_IP,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+  REDIS_URL,
+  SESSION_SECRET,
+  REDIS_PORT,
+} = require("./config/config");
+
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+// NOTE: Inifinite loop to Reconnect mongodb if has error or when db is ready
+const connectWithRetry = () => {
+  // the "@mongo:27017" below, replace ip address with mongo, as mongo is run within the same docker service
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+app.use(express.json());
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hi world!!!</h2>");
+  res.send("<h2>Hi world!!</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000;
 
